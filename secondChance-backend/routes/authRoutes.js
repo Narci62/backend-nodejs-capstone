48e8683da6e059999a4a376bc09a695743f2d5ff
kodonou narcisse <narcissekodonou@gmail.com>
2025-04-06 17:57:29 -0400

    login save

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -50,4 +50,42 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        // Task 1: Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
+        // Task 2: Access MongoDB `users` collection
+        const collection = await db.collection("users");
+        // Task 3: Check for user credentials in database
+        const theUser = await collection.findOne({email:req.body.email});
+        if(theUser){
+            let result = await bcryptjs.compare({req.body.password,theUser.password});
+            if(!result){
+                logger.error('Passwords do not match');
+                return res.status(404).jon({error:"Wrong password"}):
+            }
+
+            const userName = theUser.firstName;
+            const userEmail = theUser.email;
+
+            let payload = {
+                user:{
+                    id: theUser._id.toString(),
+                },
+            }
+
+            const authtoken = jwt.sign(payload, JWT_SECRET);
+            logger.info('User logged in successfully');
+            return res.status(200).json({authtoken,userName,userEmail})
+        }
+        else{
+            logger.error('User not found');
+            return res.status(404).json({error:"User not found"});
+        }
+    } catch (e) {
+         return res.status(500).send('Internal server error');
+
+    }
+});
+
 module.exports = router;

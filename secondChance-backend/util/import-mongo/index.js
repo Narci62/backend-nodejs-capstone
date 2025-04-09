f96aa01685cff0b1eeb05d7dc4e1faa7eb95ef6e
KODONOU narcisse <narcissekodonou@mail.com>
2025-04-09 11:07:53 +0100

    upd util index

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -1,26 +1,25 @@
-require('dotenv').config()
-const MongoClient = require('mongodb').MongoClient
-const path = require('path')
-const fs = require('fs')
+require("dotenv").config()
+const MongoClient = require("mongodb").MongoClient
+const path = require("path")
+const fs = require("fs")
 
 // MongoDB connection URL with authentication options
 const url = `${process.env.MONGO_URL}`
-const filename = path.join(__dirname, 'secondChanceItems.json')
-const dbName = 'secondChance'
-const collectionName = 'secondChanceItems'
+const filename = path.join(__dirname, "secondChanceItems.json")
+const dbName = "secondChance"
+const collectionName = "secondChanceItems"
 
 // notice you have to load the array of items into the data object
-const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs
+const data = JSON.parse(fs.readFileSync(filename, "utf8")).docs
 
 // connect to database and insert data into the collection
 
-async function loadData(){
+async function loadData() {
   const client = new MongoClient(url)
-
   try {
     // Connect to the MongoDB client
     await client.connect()
-    console.log('Connected successfully to server')
+    console.log("Connected successfully to server")
 
     // database will be created if it does not exist
     const db = client.db(dbName)
@@ -33,9 +32,9 @@ async function loadData(){
     if (documents.length === 0) {
       // Insert data into the collection
       const insertResult = await collection.insertMany(data)
-      console.log('Inserted documents:', insertResult.insertedCount)
+      console.log("Inserted documents:", insertResult.insertedCount)
     } else {
-      console.log('Items already exists in DB')
+      console.log("Items already exists in DB")
     }
   } catch (err) {
     console.error(err)
@@ -48,5 +47,5 @@ async function loadData(){
 loadData()
 
 module.exports = {
-  loadData
+  loadData,
 }
